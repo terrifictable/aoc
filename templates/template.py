3002e510983f2a4505cdd6735102f5c23700b278
TerrificTable <omegagaming189@gmail.com>
2023-11-30 15:26:09 +0100

    python template

---
+++ templates/template.py
@@ -0,0 +1,20 @@
+from collections import Counter
+from typing import List
+
+
+
+def part1(ll: list[str]):
+    return 0
+
+def part2(ll: list[str]):
+    return 0
+
+
+
+
+if __name__ == "__main__":
+    ll = [x for x in open("input.txt").read().strip().split("\n")]
+
+    print("Part 1: ", part1(ll))
+    print("Part 2: ", part2(ll))
+
